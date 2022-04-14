e1ffcc0804d3fbc575502da436c1944bd25cac54
HyperNylium <davidpivik123@gmail.com>
2022-04-14 22:16:59 +0300

    main

---
+++ app.js
@@ -0,0 +1,20 @@
+const btn1 = document.querySelector("[data-btn-1]")
+const btn2 = document.querySelector("[data-btn-2]")
+const btn3 = document.querySelector("[data-btn-3]")
+const btn4 = document.querySelector("[data-btn-4]")
+
+btn1.addEventListener("click", () => {
+    window.location.href='#' // i put a "#" for the link but you can put what ever you want //
+})
+
+btn2.addEventListener("click", () => {
+    window.location.href='##' // i put a "##" for the link but you can put what ever you want //
+})
+
+btn3.addEventListener("click", () => {
+    window.location.href='###' // i put a "###" for the link but you can put what ever you want //
+})
+
+btn4.addEventListener("click", () => {
+    window.location.href='####' // i put a "####" for the link but you can put what ever you want //
+})
